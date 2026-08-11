422169718f4ba57aff54946ba91d99bef32c079d
Matej Folta <propojocz@gmail.com>
2026-08-11 09:45:33 +0200

    velikost loga na tel

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -76,7 +76,7 @@ export default async function Navbar() {
               <img
                 src="/propojo-logo-full.png"
                 alt="Propojo"
-                className="h-12 w-auto object-contain md:h-14"
+                className="h-18 w-auto object-contain md:h-20"
               />
             </Link>
           </div>
